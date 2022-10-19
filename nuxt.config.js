62f7d9fac15475bf287fe507212eb213c0e2bda3
caramelribon <caramelribon137@gmail.com>
2022-10-19 17:14:36 +0900

    firebaseの導入
    試し中

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ["~/plugins/firebase.js",],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -40,6 +40,7 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     "@nuxtjs/axios",
+    "@nuxtjs/dotenv",
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
---
+++ pages/index.vue
@@ -0,0 +1,56 @@
+<template>
+ <div class="page">
+   <label>
+     <span>
+       お名前:
+     </span>
+     <input
+       type="text"
+       v-model="user.name"
+     >
+   </label>
+   <label>
+     <span>
+       email:
+     </span>
+     <input
+       type="text"
+       v-model="user.email"
+     >
+   </label>
+   <button
+     type="button"
+     @click="submit"
+   >
+     Submit
+   </button>
+ </div>
+</template>
+
+<script>
+import firebase from '~/plugins/firebase'
+
+export default {
+ data () {
+   return {
+     user: {
+       name: "",
+       email: ""
+     },
+   }
+ },
+ methods: {
+   submit() {
+     console.log(this.user.name, this.user.email)
+     const db = firebase.firestore()
+     let dbUsers = db.collection('users')
+     dbUsers
+       .doc('1')
+       .set({
+         name: this.user.name,
+         email: this.user.email,
+       })
+   },
+ },
+}
+</script>
