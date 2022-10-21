ef654a3dacf52bef35a8975f5d7d6ec5b191a91b
caramelribon <caramelribon137@gmail.com>
2022-10-21 17:13:10 +0900

    Footerコンポーネントの作成

---
+++ components/appFooter.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- footer -->
+  <footer class="navypink-bg h-auto">
+    <p class="text-beige text-center p-5 lora">
+      <i class="far fa-copyright copyright"></i>
+      2022 Wakana T
+    </p>
+  </footer>
+</template>
+
+<script>
+export default {
+}
+</script>
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Antic+Didone&family=Elsie&family=Italiana&family=Kaisei+Decol:wght@400;500;700&family=Lobster&family=Lora:wght@400;700&family=Parisienne&display=swap');
+/* background */
+.navypink-bg {
+  background-color: #a76e63;
+  background: linear-gradient(to right, #ab766b, #9f6054, #945447);
+}
+/* text font */
+.lora {
+  font-family: 'Lora', serif;
+}
+/* copyright */
+.copyright {
+  color: #f2ebe5;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,19 @@
   <div>
     <app-top></app-top>
     <app-message></app-message>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
 import AppMessage from '~/components/appMessage.vue'
 import AppTop from '~/components/appTop.vue'
+import AppFooter from '~/components/appFooter.vue'
 export default {
   comments: {
     AppTop,
     AppMessage,
+    AppFooter,
   }
 }
 </script>
