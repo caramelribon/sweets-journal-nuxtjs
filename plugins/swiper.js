f1d46e1fa3259145dcffd292969698382346c1c1
caramelribon <caramelribon137@gmail.com>
2022-11-06 17:13:06 +0900

    swiper.jsの修正

--- plugins/swiper.js
+++ plugins/swiper.js
@@ -1,5 +1,15 @@
 import Vue from "vue";
-import VueAwesomeSwiper from "vue-awesome-swiper";
-import 'swiper/css/swiper.css';
+import {
+  Swiper as SwiperClass,
+  Pagination,
+  Navigation,
+  Mousewheel,
+  Autoplay,
+} from "swiper/swiper.esm";
+import getAwesomeSwiper from "vue-awesome-swiper/dist/exporter";
 
-Vue.use(VueAwesomeSwiper);
+// import style
+import "swiper/swiper-bundle.min.css";
+
+SwiperClass.use([Pagination, Mousewheel, Navigation, Autoplay]);
+Vue.use(getAwesomeSwiper(SwiperClass));
